5ce05a5ac51ffbf457794ac9b723e5ad84770ebf
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2024-07-21 07:53:22 -0400

    feat: validate if the user can load the project

---
+++ src/app/shared/guards/project-validator.guard.ts
@@ -0,0 +1,41 @@
+import {Injectable} from "@angular/core";
+import {AuthService} from "../services/auth.service";
+import {ManagerService} from "../services/manager.service";
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from "@angular/router";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {Observable, tap} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectValidatorGuard {
+  constructor(
+    private authService: AuthService,
+    private managerService: ManagerService,
+    private router: Router,
+    private message: NzMessageService,
+
+  ) {
+  }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    const projectId = this.managerService.project.project_id;
+    const user = this.authService.user.id;
+    console.log('ProjectValidatorGuard', projectId, user);
+
+    return this.managerService.checkMember({
+      project_id: projectId,
+      id: user,
+    }).pipe(
+      tap((valid) => {
+        if (!valid) {
+          this.router.navigateByUrl('/dashboard');
+          this.message.error('No tienes permisos para acceder a este proyecto.');
+        }
+      }
+    ));
+  }
+
+  }
