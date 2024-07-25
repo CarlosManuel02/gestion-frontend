93a86cf2eeb2094a2e8e1175a29c9791d1edfd80
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2024-07-24 21:03:40 -0400

    WIP: CREATE a task

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  ApiEndPoint: "http://localhost:8080/api/",
+  // ApiEndPoint: "http://localhost:8080/api/",
+  ApiEndPoint: "https://gestion-server-carlos-std-07ee791a.koyeb.app/api/",
+
 };
