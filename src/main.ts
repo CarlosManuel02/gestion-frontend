a97a983c78ce6e3f5018e653822e67ee137c4825
Carlos Manuel Cedeño Barrera <carlosmcb02@gmail.com>
2024-08-02 21:34:35 -0400

    feat: Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {environment} from "./environments/environment";
 import {enableProdMode} from "@angular/core";
-
+import { inject } from "@vercel/analytics"
 if (environment.production) {
   enableProdMode();
 }
 
+inject();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
